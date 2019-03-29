65c4dab842317c086fadefbd27c366762527e559
Kamil Pytlinski <kpytlinski@gmail.com>
2019-03-29 21:39:56 +0100

    logfile

--- AddEntryToHosts.sh
+++ AddEntryToHosts.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #Zmienne
+log=/tmp/add_entry_to_hosts.log
 ip_address="192.168.1.100"
 host_name="lucy"
 host_entry="${ip_address}   ${host_name}"
@@ -10,10 +11,10 @@ host_entry="${ip_address}   ${host_name}"
 if [ `egrep -i "$ip_address|$host_name" /etc/hosts|wc -l` -eq 0 ]
 then
     echo $host_entry
-    echo "wpis nie istnieje, dodaje wpis"
+    echo "wpis nie istnieje, dodaje wpis" >>
 else
     echo $host_entry
-    echo "wpis istnieje nic nie robie"
+    echo "wpis istnieje, koncze skrypt"
     exit 2
 fi
 
