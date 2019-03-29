c95d77439d7a7917ab06358e74bbc9d8524008da
Kamil Pytlinski <kpytlinski@gmail.com>
2019-03-29 19:26:43 +0100

    cos tam dopisalem

--- AddEntryToHosts.sh
+++ AddEntryToHosts.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 #Zmienne
-ip_address="192.168.x.x"
-host_name="my.hostname.example.com"
+ip_address="192.168.1.100"
+host_name="lucy"
+host_entry="${ip_address}   ${host_name}"
 
-host_entry="${ip_address}   ${host_name}"g
+#   statusy bledow
+#   exit 2  -   wpis istnieje
+
+if [ `egrep -i $host_entry /etc/host |wc -l` -eq 0 ]
+then
+    echo "wpis nie istnieje, dodaje wpis"
+else
+    echo "wpis istnieje nic nie robie"
+    exit 2
+done
