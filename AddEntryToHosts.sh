a4247ce8a236fd0f78a878a9b2e39dd4333861d0
Kamil Pytlinski <kpytlinski@gmail.com>
2019-03-30 07:04:05 +0100

    dodanie tabulatora

--- AddEntryToHosts.sh
+++ AddEntryToHosts.sh
@@ -3,7 +3,7 @@
 log=/tmp/add_entry_to_hosts.log
 ip_address="192.168.1.100"
 host_name="lucy"
-host_entry="${ip_address}   ${host_name}"
+host_entry="${ip_address}\t${host_name}"
 
 #   statusy bledow
 #   exit 2  -   wpis istnieje
