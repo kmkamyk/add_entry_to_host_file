bd62469bc816a4d1fadca1395bcc498f652c563e
Kamil Pytlinski <kpytlinski@gmail.com>
2019-03-30 09:48:04 +0100

    dodanie logowania

--- AddEntryToHosts.sh
+++ AddEntryToHosts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Zmienne
-log=/tmp/add_entry_to_hosts.log
+log_file=/tmp/add_entry_to_hosts.log
 ip_address="192.168.1.xxx"
 host_name="xxx"
 host_entry="${ip_address}\t${host_name}"
@@ -11,11 +11,11 @@ host_entry="${ip_address}\t${host_name}"
 if [ `egrep -i "$ip_address|$host_name" /etc/hosts|wc -l` -eq 0 ]
 then
     echo $host_entry
-    echo "wpis nie istnieje, dodaje wpis" >>
+    echo "wpis nie istnieje, dodaje wpis" >> log_file
     awk 'FNR==NR{ if (/test/) p=NR; next} 1; FNR==p{ print "$host_entry" }' /etc/hosts
 else
     echo $host_entry
-    echo "wpis istnieje, koncze skrypt"
+    echo "wpis istnieje, koncze skrypt" >> log_file
     exit 2
 fi
 
